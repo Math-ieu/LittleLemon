1fca86a9a64f4f845905e1a4e1858e5dfd17d664
Math-ieu <atsoumathieu228@gmail.com>
2024-11-17 20:42:02 +0100

    update 5

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'admindjango',
+        'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
